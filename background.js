99e48542d6aa0f7cdaf6ad94c8bdc9f96a672b4b
walty8 <walty8@gmail.com>
2020-05-03 08:17:57 +0800

    use dropbox url for testing

--- background.js
+++ background.js
@@ -1,13 +1,17 @@
 var CLIENT_ID = "38131814991-p4u809qrr5ee1bsehregd4os69jf2n7i.apps.googleusercontent.com";
 var SCOPE = 'https://www.googleapis.com/auth/drive.file';
 var startAuthorize = function(){
-  var redirctURL = chrome.identity.getRedirectURL();
-  chrome.identity.launchWebAuthFlow(
-    {"url": "https://accounts.google.com/o/oauth2/auth?" +
+  var redirectURL = chrome.identity.getRedirectURL();
+  var oauthUrl = "https://accounts.google.com/o/oauth2/auth?" +
               "client_id=" + CLIENT_ID + "&scope=" + SCOPE +
-              "&redirect_uri=" + chrome.identity.getRedirectURL() +
+              "&redirect_uri=" + redirectURL +
               "&response_type=code" + 
-              "&access_type=offline",
+              "&access_type=offline";
+
+  oauthUrl = "https://www.dropbox.com/oauth2/authorize?client_id=f7by2h34w1s2b3v&response_type=code";
+
+  chrome.identity.launchWebAuthFlow(
+    {"url": oauthUrl,
      "interactive": true
     },
     function(code) {
